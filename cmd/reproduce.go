25f4ed64203252abe42067ab7590bddcb83f08be
rungsikorn.r <rungsikorn.r@me.com>
2019-12-11 11:26:19 +0700

    init

---
+++ cmd/reproduce.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"lang.yottadb.com/go/yottadb"
+	"time"
+)
+type A struct {
+	S string
+}
+func main() {
+
+	err := yottadb.SetValE(yottadb.NOTTP, nil, "something", "TEST", []string{"0001"})
+	if err != nil {
+		panic(err)
+	}
+
+	// uncomment this line for make it work as expect
+	yottadb.Exit()
+
+	go func() {
+		time.Sleep(time.Second)
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("panic recovery!")
+				fmt.Println(r)
+			}
+		}()
+		var a *A
+		fmt.Println(a.S)
+	}()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zapkub/yotta-reproduce-KILLBYSIGSINFO1
+
+go 1.13
+
+require lang.yottadb.com/go/yottadb v1.0.0
