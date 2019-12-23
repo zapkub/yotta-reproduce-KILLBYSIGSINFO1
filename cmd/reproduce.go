1dbbbb0a50ab03837d371e6f2cd075febde58b3e
rungsikorn.r <rungsikorn.r@me.com>
2019-12-23 14:43:37 +0700

    add readme

--- cmd/reproduce.go
+++ cmd/reproduce.go
@@ -8,14 +8,10 @@ type A struct {
 	S string
 }
 func main() {
-
 	err := yottadb.SetValE(yottadb.NOTTP, nil, "something", "TEST", []string{"0001"})
 	if err != nil {
 		panic(err)
 	}
-
-	// uncomment this line for make it work as expect
-	// yottadb.Exit()
 
 	defer func() {
 		if r := recover(); r != nil {
---
+++ readme.md
@@ -0,0 +1,2 @@
+## Reproduct of YottaDB Golang wrapper kill signa
+https://gitlab.com/YottaDB/Lang/YDBGo/issues/25
